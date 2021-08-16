f5d58088ed9303bad386867bf4749d27f33b4f9e
Junc <838480981@qq.com>
2021-08-16 22:13:37 +0800

    mapGetters

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import mutations from "./mutations";
+import getters from "./getters";
 import actions from "./actions";
 
 Vue.use(Vuex);
@@ -12,6 +13,7 @@ const state = {
 const store = new Vuex.Store({
   state,
   mutations,
+  getters,
   actions
 });
 
--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,15 +1,39 @@
 <template>
-  <div>
-    <h2>购物车</h2>
+  <div class="cart">
+    <nav-bar class="cart-nav">
+      <template #center>购物车({{ length }})</template>
+    </nav-bar>
+    <ul>
+      <li v-for="(item, index) in list" :key="index">
+        <img width="100%" :src="item.image" alt="" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Cart'
-  }
+import { mapGetters } from "vuex";
+
+import NavBar from "components/common/navbar/NavBar";
+export default {
+  name: "Cart",
+  components: {
+    NavBar,
+  },
+  computed: {
+    // ...mapGetters(["cartLength", "cartList"]),
+    // 自定义名称用法
+    ...mapGetters({
+      length: "cartLength",
+      list: "cartList",
+    }),
+  },
+};
 </script>
 
 <style scoped>
-
+.cart-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
